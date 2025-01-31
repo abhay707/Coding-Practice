4d0376f650967ce652350ac2b32ff81c06a66394
Abhay Chaturvedi <abhx1437@gmail.com>
2025-01-31 18:58:15 +0530

    Time: 16 ms (65.76%), Space: 58.2 MB (68.23%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int length(ListNode* head){
+        int cnt = 0;
+        ListNode* cur = head;
+        while(cur){
+            cur = cur->next;
+            cnt++;
+        }
+        return cnt;
+    }
+    
+    ListNode* midPoint(ListNode* head){
+        if(length(head) < 2) return head;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* prev = nullptr;
+
+        while(fast && fast->next){
+            prev = slow;
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        if (prev) prev->next = nullptr;
+        return slow;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
+        if(list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL) return head;
+        if(head->next == NULL) return head;
+        ListNode* mid = midPoint(head);
+        ListNode* head1 = head;
+        ListNode* head2 = mid;
+        
+        head1 = sortList(head1);
+        head2 = sortList(head2);
+        return mergeTwoLists(head1, head2);
+    }
+};
